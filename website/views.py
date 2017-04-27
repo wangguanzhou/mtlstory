67cd24ba69a753cf3daf5058158f3da26540a6dd
Guanzhou Wang <alexwang74@gmail.com>
2017-04-26 22:32:12 -0400

    ..

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 # Create your views here.
 
