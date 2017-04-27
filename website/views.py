7fa524b0e8a016b16dfce580b927e85dee0db077
Guanzhou Wang <alexwang74@gmail.com>
2017-04-26 21:55:01 -0400

    complete login function

--- website/views.py
+++ website/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+ErrMsgs = {
+	'admin-login-error': '用户名或者密码错误，请重新尝试登录。',
+}
+
 def homepage(request):
 	context = {}
 	return render(request, 'homepage.html', context)
@@ -10,4 +14,28 @@ def homepage(request):
 
 def adminlogin(request):
 	context = {}
-	return render(request, 'adminlogin.html', context)
+	if request.user.is_authenticated:
+		context['authenticated'] = True
+		context['heading'] = '管理员功能'
+		username = request.user.username
+		return render(requeset, 'adminlogin.html', context)
+	elif request.POST:
+		if 'login-btn' in request.POST:
+			username = request.POST['admin-name']
+			password = request.POST['admin-pwd']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				context['authenticated'] = True
+				context['heading'] = '管理员功能'
+				context['username'] = request.user.username
+				return render(requeset, 'adminlogin.html', context)
+			else: 
+				context['authenticated'] = False
+				context['heading'] = '管理员登录'
+				context['errmsg'] = ErrMsgs['admin-login-error']
+				return render(requeset, 'adminlogin.html', context)
+	else:
+		context['authenticated'] = False
+		context['heading'] = '管理员登录'
+		return render(request, 'adminlogin.html', context)
