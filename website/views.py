ea98b21a75e086e4100a1939ed7e10ba12f8f03c
Guanzhou Wang <alexwang74@gmail.com>
2017-04-28 21:49:47 -0400

    more create notice forms

--- website/views.py
+++ website/views.py
@@ -71,5 +71,29 @@ def createnotice(request):
 		context['authenticated'] = True
 		context['username'] = username
 		context['district_name'] = district
-		context['heading'] = '管理员功能 - 创建新故事会通知'
+		context['heading'] = '创建新故事会通知'
+		default_notice = set_default_notice(district)
+		context.update(default_notice)
 		return render(request, 'createnotice.html', context)
+
+
+
+def set_default_notice(district):
+    places = {
+        'Brossard': 'Bibliothèque de Brossard (Brossard图书馆儿童活动区)',
+        'Longueuil': 'Bibliothèque Georges-Dor',
+        'CDN': 'CDN图书馆儿童活动室（地下一层）',
+        'Montreal': 'Montreal library'
+    }
+    addresses = {
+        'Brossard': '7855 Ave San Francisco, Brossard J4X 2A4',
+        'Longueuil': '2760 chemin de Chambly, Longueuil J4L 1M6',
+        'CDN': '5290 Chemin de la Côté-des-Neiges, Montréal H3T 1Y3',
+        'Montreal': '5290 Chemin de la Côté-des-Neiges, Montréal H3T 1Y3',
+    }
+
+    default_notice = {}
+	default_notice['story_maxsize'] = 20
+	default_notice['story_site'] = places[district]
+	default_notice['story_address'] = addresses[district]
+
