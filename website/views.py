4093a3fd5344843ece0a17f273eba8958492e637
Guanzhou Wang <alexwang74@gmail.com>
2017-05-02 22:08:26 -0400

    ...

--- website/views.py
+++ website/views.py
@@ -190,7 +190,7 @@ def set_default_notice(district):
 	default_notice['activity-1']['activity_info'] = '小朋友们自我介绍； 一起唱《你好歌》。'
 	default_notice['activity-1']['activity_img_url'] = ''
 
-	for activity_name in ['activity-2', 'activity-3', 'activity-4', 'activity-5' ]
+	for activity_name in ['activity-2', 'activity-3', 'activity-4', 'activity-5']:
 		default_notice[activity_name] = {}
 		default_notice[activity_name]['activity_info'] = ''
 		default_notice[activity_name]['activity_img_url'] = ''
